da0fc4f2c1388b31c8c673b912e21ae2d63ae7cc
Mr-RoB0T-whoami <guevarasalcedotatianaguadalupe@gmail.com>
2026-05-15 23:12:59 -0500

    e añadido seno coseno y tangente

--- main.lua
+++ main.lua
@@ -18,13 +18,21 @@ function potenciacion(a,b)
 end
 function  rais(a,b)
     return a ^ (1 / b)
-    
+end
+function seno(a)
+    return math.sin(a)
+end
+function coseno(a)
+    return math.cos(a)
+end
+function tangent(a)
+    return math.tan(a)
 end
 
 print("elige dos numeros")
 local n1 = tonumber(io.read())
 local n2 = tonumber(io.read())
-print("elige la operasion: +,*,-,/,pot,rais")
+print("elige la operasion: +,*,-,/,pot,rais,seno,coseno,tangent")
 local operasion = io.read()
 if operasion == "+" then
     print(suma(n1,n2))
@@ -41,4 +49,11 @@ elseif operasion=="pot" then
     print(potenciacion(n1,n2))
 elseif operasion=="rais" then
     print(rais(n1,n2))
+elseif operasion== "seno" then
+    print(seno(n1))
+elseif operasion== "coseno" then
+    print(coseno(n1))
+elseif operasion== "tangent" then
+    print(tangent(n1))
 end
+
